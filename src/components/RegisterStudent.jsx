4716787334c4ded35a0998dddc481b8f107db400
raymayo <ormillada.jr@gmail.com>
2024-08-18 05:05:38 +0800

    fix

--- src/components/RegisterStudent.jsx
+++ src/components/RegisterStudent.jsx
@@ -98,7 +98,7 @@ const RegisterStudent = () => {
 							  ctx.drawImage(img, 0, 0);
 							  const pngFile = canvas.toDataURL("image/png");
 							  const downloadLink = document.createElement("a");
-							  downloadLink.download = "QRCode";
+							  downloadLink.download = `${formData.studentName} "QRCode"`;
 							  downloadLink.href = `${pngFile}`;
 							  downloadLink.click();
 							};
